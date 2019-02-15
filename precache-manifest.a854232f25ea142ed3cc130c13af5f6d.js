69295b843ffffd8eb6e9b2326ca3fc7fc3776449
ulaulala <ukazmierczyk@gmail.com>
2019-02-15 16:05:08 +0100

    Updates

---
+++ precache-manifest.a854232f25ea142ed3cc130c13af5f6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8a4f21e54edad9dbc140",
+    "url": "/static/js/main.8a4f21e5.chunk.js"
+  },
+  {
+    "revision": "dae44800af2041ed1bb3",
+    "url": "/static/js/2.dae44800.chunk.js"
+  },
+  {
+    "revision": "8a4f21e54edad9dbc140",
+    "url": "/static/css/main.47aef161.chunk.css"
+  },
+  {
+    "revision": "e7b7d2278d115df324c8431163ba89bd",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b57fccae1252c570d57fd64b772ebfce.js"
+  "/precache-manifest.a854232f25ea142ed3cc130c13af5f6d.js"
 );
 
 workbox.clientsClaim();
