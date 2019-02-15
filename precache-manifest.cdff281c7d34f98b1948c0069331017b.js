4b5f5a37971b289bd2a4477bda330826a378b160
Piotr Zientara <314zientara@gmail.com>
2019-02-15 11:41:12 +0100

    Updates

---
+++ precache-manifest.cdff281c7d34f98b1948c0069331017b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/puk-puk/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "53fcdde3a867b4364cae",
+    "url": "/puk-puk/static/js/runtime~main.53fcdde3.js"
+  },
+  {
+    "revision": "f5c895767c551aca5a2c",
+    "url": "/puk-puk/static/js/main.f5c89576.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/puk-puk/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "f5c895767c551aca5a2c",
+    "url": "/puk-puk/static/css/main.49f17be2.chunk.css"
+  },
+  {
+    "revision": "27fadd3a5918110787b6eb2b1ce2f2e4",
+    "url": "/puk-puk/index.html"
+  }
+];
