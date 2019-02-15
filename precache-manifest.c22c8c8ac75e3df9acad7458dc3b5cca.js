a0f609bafcd1ed36c5fb8e14fbaa2135faf632cb
Piotr Zientara <314zientara@gmail.com>
2019-02-15 13:10:52 +0100

    Updates

---
+++ precache-manifest.c22c8c8ac75e3df9acad7458dc3b5cca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53fcdde3a867b4364cae",
+    "url": "/puk-puk/static/js/runtime~main.53fcdde3.js"
+  },
+  {
+    "revision": "98cf937b56ff0b502510",
+    "url": "/puk-puk/static/js/main.98cf937b.chunk.js"
+  },
+  {
+    "revision": "6091588e3e803e53a246",
+    "url": "/puk-puk/static/js/2.6091588e.chunk.js"
+  },
+  {
+    "revision": "98cf937b56ff0b502510",
+    "url": "/puk-puk/static/css/main.5452e882.chunk.css"
+  },
+  {
+    "revision": "8a2a6eae2ae26ebc87e338a7568ea014",
+    "url": "/puk-puk/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ea0f5b11e61c39d47f75d167fbe4057e.js"
+  "/puk-puk/precache-manifest.c22c8c8ac75e3df9acad7458dc3b5cca.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/puk-puk/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
